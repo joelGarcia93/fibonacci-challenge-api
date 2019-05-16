2660272d11f9d32ea8d7e1ca36b3edb746bae2a4
Joel Garcia <joel.java93@gmail.com>
2019-05-15 20:12:52 -0500

    changed imports for es6 syntax

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
+import express from 'express';
+import bodyParser from 'body-parser';
+import fibonacciRoutes from './routes/fibonacci';
 
-const fibonacciRoutes = require('./routes/fibonacci');
+var app = express();
 
 app.use(bodyParser.urlencoded({ extended:false }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Headers',
+      'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
